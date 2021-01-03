a576f1b7755d2cab76e0a7c2d593c7c23e4c25e7
leelmamta <69026100+leelmamta@users.noreply.github.com>
2021-01-03 08:43:32 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,13 @@
+import turtle
+def square(): #defining function 
+   quaz_turtle = turtle.Turtle()
+   quaz_turtle.forward(100)
+   quaz_turtle.right(90)
+   quaz_turtle.forward(100)
+   quaz_turtle.right(90)
+   quaz_turtle.forward(100)
+   quaz_turtle.right(90)
+   quaz_turtle.forward(100)
+   quaz_turtle.right(90)
+
+square()   # calling function 
