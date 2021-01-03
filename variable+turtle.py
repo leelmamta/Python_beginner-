749a09aba6ff1ef960256bcfa8407d8af756563b
leelmamta <69026100+leelmamta@users.noreply.github.com>
2021-01-03 08:42:54 +0530

    Add files via upload

---
+++ variable+turtle.py
@@ -0,0 +1,16 @@
+import turtle
+a = 6
+print(a)
+credit_card = 98394723
+print(credit_card)
+b="dfhg"
+print(b)
+quaz_turtle = turtle.Turtle()
+quaz_turtle.forward(100)
+quaz_turtle.right(90)
+quaz_turtle.forward(100)
+quaz_turtle.right(90)
+quaz_turtle.forward(100)
+quaz_turtle.right(90)
+quaz_turtle.forward(100)
+quaz_turtle.right(90)
